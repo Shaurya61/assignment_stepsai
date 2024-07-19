e7d1f5aaac0efa6383f4faaeea173fdf2bcb0f86
Shaurya <shauryanayyarviic30@gmail.com>
2024-07-19 16:57:32 +0530

    fix signout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,7 +48,7 @@ const Header = () => {
             <Link href="/doctor/dashboard" className="block px-4 py-2 hover:underline">Dashboard</Link>
             <Link href="/doctor/dashboard/uploaded-pdf" className="block px-4 py-2 hover:underline">Uploaded PDFs</Link>
             <Link href="/doctor/dashboard/linked-patients" className="block px-4 py-2 hover:underline">All Linked Patients</Link>
-            <Link href="/" className="block px-4 py-2 hover:underline" onClick={handleSignOut}>Signout</Link>
+            <Link href="/" className="block px-4 py-2 hover:underline" onClick={handleSignOut}>Signout</Link> 
           </div>
           <div className="flex-1" onClick={closeMenu}></div>
         </div>
